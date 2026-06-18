37e8710c569ff827437b0aa7bb74eb2de62b6560
bbatistadaniel <danielbatista1729@gmail.com>
2026-06-18 09:17:36 -0300

    empty flutter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# fro3
+
+A new Flutter project.
---
+++ android/app/src/main/java/br/com/bbatistadaniel/fro3/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.bbatistadaniel.fro3;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
