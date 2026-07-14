04cc3ff7692f44032bcea61b4cdc6ea46143ba11
bbatistadaniel <danielbatista1729@gmail.com>
2026-07-14 17:08:34 -0300

    altered the package from 'br.com.bbatistadaniel.fro3' to 'dev.bbatistadaniel.fro3'

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "br.com.bbatistadaniel.fro3"
+        applicationId = "dev.bbatistadaniel.fro3"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
